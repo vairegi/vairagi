62767de6162c78c771f4fcd266e59d7d20a2a4e7
vairegi <162858970+vairegi@users.noreply.github.com>
2024-03-14 19:55:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 rm -rf boted;
-git clone https://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@github.com/TGthings/Jerry boted;
-cd boted;
+git clone https://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@github.com/vairegi/vairagi/vairagi
+cd/vairagi
 pip3 install -U -r requirements.txt;
 echo "Bot Started....";
 python3 bot.py;
